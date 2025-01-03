cf401cf4d78d45d4e58512e02920a03827427c50
BeneX Drake <benex@hotmail.de>
2025-01-03 16:59:26 +0100

    working on modal

---
+++ src/components/anime_modal.ts
@@ -0,0 +1,11 @@
+import { Anime } from "../types/anime";
+
+export let animeModal = (anime:Anime) =>
+{
+    let modal = document.getElementById('anime_modal') as HTMLDivElement
+
+    if(!modal) return;
+
+    
+
+}
